7e061687554379a41ea29b17e86c62920af9b4dd
Łukasz <lukaskapron@gmail.com>
2022-06-13 13:53:59 +0200

    Inicjalizacja projektu: 	Dodanie pierwszej wersji GUI 	Generowanie macierzy incydencji

---
+++ Grafy/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Grafy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 165);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(652, 366);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(83, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(518, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Podaj ilość wierzchołków grafu skierowanego";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(184, 56);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(134, 27);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zatwierdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(673, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Szukaj najkrótszego cyklu";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(83, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(514, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Podaj macierz incydencji grafu skierowanego";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(685, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Najkrótszy cykl:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(685, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "1-2-3-4-5-6-1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 573);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Grafy Projekt";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Grafy/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        TextBox GenerujTxt(int i)
+        {
+            TextBox tb = new TextBox();
+            tb.Name = "Textbox" + i.ToString();
+            tb.Width = 40;
+            tb.Height = 40;
+            return tb;
+        }
+        Label GenerujLabel(int i)
+        {
+            Label l = new Label();
+            l.Name = "Label" + i.ToString();
+            l.Width = 40;
+            l.Height = 40;
+            l.Text = i.ToString();
+            l.Margin = new Padding(0);
+            l.TextAlign = ContentAlignment.MiddleCenter;
+            return l;
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            int matrixSize = (int)numericUpDown1.Value;
+            int txtBoxAmount = matrixSize * matrixSize;     
+            flowLayoutPanel1.Width = (matrixSize+1) * 40;
+            int n = 0;
+            int numerwierwsza = 1;
+
+            for (int i = 0; i <= matrixSize; i++)
+            {
+                Label l = GenerujLabel(i);
+                flowLayoutPanel1.Controls.Add(l);
+                
+            }
+            for (int i = 0; i < txtBoxAmount; i++)
+            {
+                if (i==0)
+                {
+                    Label l = GenerujLabel(n+1+matrixSize);            
+                    flowLayoutPanel1.Controls.Add(l);
+                    n++;
+                    TextBox tb = GenerujTxt(i);
+                    tb.Margin = new Padding(0, 0, 0, 0);
+                    flowLayoutPanel1.Controls.Add(tb);
+                    l.Text = n.ToString();
+                }
+                else if (i>0 && i==n*matrixSize)
+                {
+                    Label l = GenerujLabel(n + 1 + matrixSize);
+                    flowLayoutPanel1.Controls.Add(l);
+                    n++;
+                    l.Text = n.ToString();
+                    TextBox tb = GenerujTxt(i);
+                    tb.Margin = new Padding(0, 0, 0, 0);
+                    flowLayoutPanel1.Controls.Add(tb);
+                }
+                else
+                {
+                    TextBox tb = GenerujTxt(i);
+                    tb.Margin = new Padding(0, 0, 0, 0);
+                    flowLayoutPanel1.Controls.Add(tb);
+                }
+                
+                
+ 
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
