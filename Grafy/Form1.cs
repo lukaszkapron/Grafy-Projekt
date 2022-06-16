e6d4a4550ec64721f879f09268984dd0bdde634b
Łukasz <lukaskapron@gmail.com>
2022-06-16 17:01:43 +0200

    Dodanie algorytmu szukania cykli 	Dodanie komentarzy

--- Grafy/Form1.cs
+++ Grafy/Form1.cs
@@ -10,6 +10,14 @@ using System.Windows.Forms;
 
 namespace Grafy
 {
+	// Co trzeba zrobić:
+	// 1. -zdecydować czy dajemy możliwość wpisawania macierzy sąsiedztwa czy tylko liste wierzchołków
+	// 2. Zrobić tak, żeby po kliknięciu szukaj cyklu zostały dodane krawędzie funkcją addEdge
+	// 3. Obsłużyć błędy czyli gdy nie wszystkie krawędzie wpisane itp.
+	// 4. Obsłużyć przycisk szukaj cyklu żeby szukał cyklu XD
+	// 5. Przemyśleć błędy użyrkownika
+	// 6. Opisać algorytm i napisać instrukcje
+	// 7. Jak coś to można poprawić GUI bo nie umiem w Design XD
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,7 +25,7 @@ namespace Grafy
             InitializeComponent();
         }
 
-
+		// Niepotrzebne ale nie da się usunąć XD
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -44,6 +52,7 @@ namespace Grafy
         }
 
         //Button generuj macierz
+		//Fajną macierz generuje chyba
         private void button1_Click_1(object sender, EventArgs e)
         {
             flowLayoutPanel1.Controls.Clear();
@@ -94,7 +103,7 @@ namespace Grafy
         private void button2_Click(object sender, EventArgs e)
         {
             List <TextBox> listaTextBoxow = new List<TextBox>();
-
+			// pętla for tworzy listę "listaTextBoxow", w której są wszystkie textboxy z macierzy lub listy krawędzi z GUI
             for (int i = 0; i < flowLayoutPanel1.Controls.Count; i++)
             {
                 if (flowLayoutPanel1.Controls[i] is TextBox)
@@ -109,6 +118,7 @@ namespace Grafy
         }
 
         // Button do Listy
+		// Tworzy listę krawędzi
         private void button3_Click(object sender, EventArgs e)
         {
             flowLayoutPanel1.Controls.Clear();
@@ -155,10 +165,177 @@ namespace Grafy
             }
 
         }
+		// To nie potrzebne ale nie da się usunąć XD
         private void label4_Click(object sender, EventArgs e)
         {
-            var x = flowLayoutPanel1.Controls.GetType(); ;
+            
         }
 
-    }
+
+
+
+		//Algortym do szukania cykli:
+
+		// Klasa Węzeł listy sąsiedztwa
+		public class AjlistNode
+		{
+			public int id;
+			public AjlistNode next; //następny sąsiad
+			public AjlistNode(int id)
+			{
+				this.id = id;
+				this.next = null;
+			}
+		}
+		// Klasa Wierzhołki
+		public class Vertices
+		{
+			public int data;
+			public AjlistNode next; //następny węzeł
+			public AjlistNode last; //poprzedni węzeł
+			public Vertices(int data)
+			{
+				this.data = data;
+				this.next = null;
+				this.last = null;
+			}
+		}
+		// klasa Graf
+		public class Graph
+		{
+
+			public int size; // Liczba wierczhołkow
+			public Vertices[] node; // Tablica zawiera Wierzchołki które mówią o tym z jakimi innymi wierzchołkami jest on połączony
+			public Graph(int size)
+			{
+				this.size = size;
+				this.node = new Vertices[size];
+				this.setData();
+			}
+			public void setData()
+			{
+				if (size <= 0)
+				{
+					Console.WriteLine("\nEmpty Graph"); // zmienić na MessageBox
+				}
+				else
+				{
+					for (int index = 0; index < size; index++)
+					{				
+						node[index] = new Vertices(index); // Początkowa wartość węzła
+					}
+				}
+			}
+			// Funkcja do łąćzenia wierchułków:
+			public void connection(int start, int last)
+			{				
+				AjlistNode edge = new AjlistNode(last); 
+				if (node[start].next == null)
+				{
+					node[start].next = edge;
+				}
+				else
+				{
+					// Dodanie edge na koniec
+					node[start].last.next = edge;
+				}
+				// Pobranie ostatniego węzła
+				node[start].last = edge;
+			}
+			//  Dodawanie nowej krawędzi:
+			public void addEdge(int start, int last)
+			{
+				if (start >= 0 && start < size &&
+					last >= 0 && last < size)
+				{
+					connection(start, last); 
+				}
+				else
+				{
+					Console.WriteLine("\nHere Something Wrong"); //Zmieniź na MessageBox
+				}
+			}
+			public void printGraph()
+			{
+				if (size > 0)
+				{
+					// Drukowanie węzła list sąsiedztwa czyli np. 1--2,3,4     2--5,6 
+					// Raczej nie jest nam potrzebna ta funkcja
+
+					for (var index = 0; index < size; ++index)
+					{
+						Console.Write("\nAdjacency list of vertex " +
+									  index + " :");
+						var edge = node[index].next;
+						while (edge != null)
+						{
+							// Display graph node 
+							Console.Write("  " + node[edge.id].data);
+							// Visit to next edge
+							edge = edge.next;
+						}
+					}
+				}
+			}
+			// Drukowanie cykli
+			// Trzeba rozważyć czy chcemy drukować każdy cykl, czy tylko jeden
+			// ta funkcja pobiera jeszcze wartość n która mowi o jakiej długości ma być cykl
+			// potrzebujey żeby znaleźć najkrótszy cykl więc trzeba to przerobić
+			public void printCycleByLength(bool[] visit, int start, int source, int length, int n, String result)
+			{
+				if (start >= size || source >= size || start < 0 || source < 0 || size <= 0)
+				{
+					return;
+				}
+				if (visit[start] == true)
+				{
+					// Here length are indicate number of edge
+					if (start == source && length == n)
+					{
+						// Display cycle of length n
+						Console.WriteLine("(" + result + ")");
+					}
+					return;
+				}
+				// Here modified the value of visited node
+				visit[start] = true;
+				// This is used to iterate nodes edges
+				var edge = node[start].next;
+				while (edge != null)
+				{
+					printCycleByLength(visit, edge.id, source, length + 1, n, result + "  " + edge.id);
+					// Visit to next edge
+					edge = edge.next;
+				}
+				// Reset the value of visited node status
+				visit[start] = false;
+			}
+			// funckaj do wywoływania funkcji drukowania cykli, też trzeba zmienić "n"
+			public void cycleByLength(int n)
+			{
+				if (n < 2 || n >= size)
+				{
+					return;
+				}
+				if (size <= 0)
+				{
+					// Empty graph
+					return;
+				}
+				// Auxiliary space which is used to store 
+				// information about visited node
+				bool[] visit = new bool[this.size];
+				Console.WriteLine("\nResult : ");
+				for (var i = 0; i < this.size; ++i)
+				{
+					// Check cycle of node i to i with length n
+					// visit - tablica odwiedzonych wiezchołków
+					// i = numer wierzchołka
+					// n == długość cyklu
+					//
+					printCycleByLength(visit, i, i, 0, n, "" + i.ToString());
+				}
+			}
+		}
+	}
 }
